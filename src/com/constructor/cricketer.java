3e3b2947cae297fdbc29989df5661ee98d23cac9
shubham <shubham@gmail.com>
2026-04-11 23:33:06 +0530

    constructor

---
+++ src/com/constructor/cricketer.java
@@ -0,0 +1,26 @@
+package com.constructor;
+
+class cricketer {
+    private String name;
+
+    cricketer(String playerName) {
+        this.name = playerName;
+    }
+
+    public void display() {
+        System.out.println("Cricketer: " + name);
+    }
+
+    public static void main(String[] args) {
+        cricketer c1 = new cricketer("Virat Kohli");
+        cricketer c2 = new cricketer("MS Dhoni");
+        cricketer c3 = new cricketer("Sachin Tendulkar");
+        cricketer c4 = new cricketer("Rohit Sharma");
+
+        c1.display();
+        c2.display();
+        c3.display();
+        c4.display();
+    }
+}
+
--- src/com/corejava/object.java
+++ src/com/corejava/object.java
@@ -20,20 +20,20 @@ public class object {
         Cricketer obj5 = new Cricketer();
 
         
-        obj1.name1 = "Mithali raj";
-        obj1.name2 = "smriti mandhana";
+        obj1.name1 = "virat kohli";
+        obj1.name2 = "rohit sharma";
 
-        obj2.name1 = "harmanpreet kaur";
-        obj2.name2 = "jhulan goswami";
+        obj2.name1 = "shreyas iyar";
+        obj2.name2 = "hardik pandya";
 
-        obj3.name1 = "deepti sharma";
-        obj3.name2 = "shafali verma";
+        obj3.name1 = "krunal pandya";
+        obj3.name2 = "suresh raina";
 
-        obj4.name1 = "richa ghosh";
-        obj4.name2 = "meg lanning";
+        obj4.name1 = "jaspreet bumrah";
+        obj4.name2 = "sachin tendulker";
 
-        obj5.name1 = "sophie lanning";
-        obj5.name2 = "ellyse perry";
+        obj5.name1 = "rajat patidar";
+        obj5.name2 = "devdutt paddikal";
 
         
         obj1.printNames();
