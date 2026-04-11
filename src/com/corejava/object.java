4b7006fba7a53eaf2e8f9ebc1b789c3549bc89f2
shubham <shubham@gmail.com>
2026-04-11 20:08:24 +0530

    core java

--- src/com/corejava/object.java
+++ src/com/corejava/object.java
@@ -12,30 +12,30 @@ class Cricketer {
 
 public class object {
     public static void main(String[] args) {
-        // Create 5 objects
+        
         Cricketer obj1 = new Cricketer();
         Cricketer obj2 = new Cricketer();
         Cricketer obj3 = new Cricketer();
         Cricketer obj4 = new Cricketer();
         Cricketer obj5 = new Cricketer();
 
-        // Assign names directly
-        obj1.name1 = "Virat Kohli";
-        obj1.name2 = "Rohit Sharma";
+        
+        obj1.name1 = "Mithali raj";
+        obj1.name2 = "smriti mandhana";
 
-        obj2.name1 = "MS Dhoni";
-        obj2.name2 = "Sachin Tendulkar";
+        obj2.name1 = "harmanpreet kaur";
+        obj2.name2 = "jhulan goswami";
 
-        obj3.name1 = "Kane Williamson";
-        obj3.name2 = "Steve Smith";
+        obj3.name1 = "deepti sharma";
+        obj3.name2 = "shafali verma";
 
-        obj4.name1 = "Ben Stokes";
-        obj4.name2 = "David Warner";
+        obj4.name1 = "richa ghosh";
+        obj4.name2 = "meg lanning";
 
-        obj5.name1 = "Jasprit Bumrah";
-        obj5.name2 = "Babar Azam";
+        obj5.name1 = "sophie lanning";
+        obj5.name2 = "ellyse perry";
 
-        // Print all names
+        
         obj1.printNames();
         obj2.printNames();
         obj3.printNames();
