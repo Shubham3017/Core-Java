2586290f463bbd13639d626de019c92db75f2165
shubham <your_verified_email@example.com>
2026-04-12 00:00:24 +0530

    Commit with correct email

--- src/com/constructor/cricketer.java
+++ src/com/constructor/cricketer.java
@@ -14,13 +14,14 @@ class cricketer {
     public static void main(String[] args) {
         cricketer c1 = new cricketer("Virat Kohli");
         cricketer c2 = new cricketer("MS Dhoni");
-        cricketer c3 = new cricketer("Sachin Tendulkar");
+        cricketer c3 = new cricketer("Sachin  Tendulkar");
         cricketer c4 = new cricketer("Rohit Sharma");
-
+        cricketer c5=  new cricketer("jaspreet bumrah");
         c1.display();
         c2.display();
         c3.display();
         c4.display();
-    }
+        c5.display();
+        }
 }
 
